b4842ff32221109ac1e6a8048531e5c9948f4bb5
Blake Wilson <blake@blake.id>
2024-03-05 19:02:09 -0600

    Setup header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
+import { Metadata, ResolvingMetadata } from "next";
 import Editor from "../components/editor";
+import EnterURL from "../components/enter-url";
 import Header from "../components/header";
+import Logo from "../components/logo";
 
 export type SearchParams = {
   url?: string;
   query?: string;
   variables?: string;
 };
+
+export async function generateMetadata(
+  { params, searchParams }: { params: any; searchParams: SearchParams },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  let title = "Graphman - Test and share GraphQL requests.";
+
+  if (searchParams.url) {
+    title = `Graphman - ${new URL(searchParams.url).host}`;
+  }
+
+  return {
+    title,
+    description: "Graphman lets you test and share GraphQL requests.",
+  };
+}
 
 export default async function Page({
   params,
@@ -17,7 +36,19 @@ export default async function Page({
   const { url, query, variables } = searchParams;
 
   if (!url) {
-    return <>No url</>;
+    return (
+      <div className="h-screen flex flex-col items-center justify-center">
+        <div className="mx-auto w-full max-w-sm">
+          <div className="mb-10 text-center">
+            <Logo />
+            <p className="mt-2 text-slate-400">
+              Test and share GraphQL requests.
+            </p>
+          </div>
+          <EnterURL />
+        </div>
+      </div>
+    );
   }
 
   return (
--- components/header.tsx
+++ components/header.tsx
@@ -1,20 +1,21 @@
 import EndpointInput from "./endpoint-input";
+import Logo from "./logo";
+import Share from "./share";
 
 export default function Header() {
   return (
     <div className="p-5">
       <div className="flex items-center">
         <div className="w-1/4">
-          <a
-            href="#"
-            className="mr-auto uppercase font-extrabold text-white text-2xl"
-          >
-            Graphman
-          </a>
+          <Logo />
         </div>
 
         <div className="w-1/2">
           <EndpointInput />
+        </div>
+
+        <div className="w-1/4 text-right">
+          <Share />
         </div>
       </div>
     </div>
